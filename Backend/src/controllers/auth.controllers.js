fb184ef8cb2e24a9d6d721930b1e0d2b8258fd51
AshishSharma975 <ashish8389sharma@gmail.com>
2026-03-06 12:00:42 +0530

    register

--- Backend/src/controllers/auth.controllers.js
+++ Backend/src/controllers/auth.controllers.js
@@ -1,3 +1,11 @@
 const userModel = require("../models/user.model")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+
+
+async function registerUser(req,res) {
+    
+}
+
+
+module.exports ={registerUser}
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,10 +1,10 @@
 const {Router, Router} = require("express")
-
+const authController = require("../controllers/auth.controllers")
 const Router = Router();
 
 
 
-Router.post('/register')
+Router.post('/register',authController.registerUser)
 
 module.exports = Router
 
